e536b67dd6f33cc1432935da0b50edbcf1658db3
Robert Yohnka <robertyohnka@Roberts-MBP.attlocal.net>
2024-05-18 23:19:56 -0400

    thru select a featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,37 @@ import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
   console.log("puppyList", puppyList);
 
+  function handleClick() {
+
+  }
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log("featuredPup: ", featuredPup);
+
   return (
-    <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p>key={puppy.id}>{puppy.name}</p>;
-        })
-      }
-    </div>
+    <>
+      <div className="App">
+        {puppies.map((puppy) => {
+          return (
+            <p
+              onClick={() => { setFeatPupId(puppy.id) }}
+              key={puppy.id}>{puppy.name}</p>
+            // console.log("puppy.id: ", puppy.id)
+          );
+        })}
+        {featPupId !== null && (
+          <div>
+            <h2>{featuredPup.name || "No Name"}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
